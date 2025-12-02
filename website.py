28a3a1204193aa9f3498583877483b945e218850
Sebastián Torres <sebastiantorres1129733@correo.itm.edu.co>
2025-12-02 13:34:50 -0500

    Se agregaron mensajes emergentes que le muestra al usuario información de sus acciones.

--- website.py
+++ website.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 
 app = Flask(__name__)
 app.secret_key = "rollheiser"
@@ -13,7 +13,8 @@ def userAccount():
         name = session["username"]
 
         return render_template("user.html", name=name)
-
+    
+    flash(message="You need to log in.", category="info")
     return redirect(url_for("login"))
 
 @app.route("/login", methods=["GET", "POST"])
@@ -22,11 +23,13 @@ def login():
         username: str = request.form["usname"]
         session["username"] = username
 
+        flash(message="Logged in succesfully!", category="info")
         return redirect(url_for("userAccount"))
     else:
         if "username" in session:
+            flash(message="Already logged in.", category="info")
             return redirect(url_for("userAccount"))
-
+        
         return render_template("login.html")
 
 @app.route("/settings", methods=["GET", "POST"])
@@ -34,6 +37,7 @@ def settings():
     if request.method == "POST":
         session.pop("username", None)
 
+        flash(message="Logged out succesfully!", category="info")
         return redirect(url_for("login"))
 
     return render_template("settings.html")
